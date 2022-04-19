aa852004eea92922eebe99da85943cf7ca1b7b36
SHDMYUA <90021141+SHDMYUA@users.noreply.github.com>
2022-04-20 01:18:12 +0300

    mobile-menu in procrss

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,12 @@
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector(".menu-open-btn"),
-    closeMenuBtn: document.querySelector(".menu-close-btn"),
-    menu: document.querySelector(".mob-menu"),
-  };
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
-
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-  }
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+    menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    menuBtnRef.classList.toggle("is-active");
+    // menuBtnRef.setAttribute("aria-expanded", !expanded);
+    mobileMenuRef.classList.toggle("is-open");
+  });
 })();
