4e9448e4936e797db005e2d95fecc935b76d74f5
SHDMYUA <90021141+SHDMYUA@users.noreply.github.com>
2022-04-20 20:33:26 +0300

    goto portfolio

--- js/menu.js
+++ js/menu.js
@@ -4,8 +4,9 @@
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
-    menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-    menuBtnRef.classList.toggle("is-active");
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
     // menuBtnRef.setAttribute("aria-expanded", !expanded);
     mobileMenuRef.classList.toggle("is-open");
   });
